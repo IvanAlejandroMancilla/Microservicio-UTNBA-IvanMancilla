c3fd7e99e44346c99514e0ea85c446ae191b15ca
IVAN <IVAN.ALEJANDRO.MANCILLA@GMAIL.com>
2026-07-27 02:52:44 -0400

    PRIMEROS PASOS

---
+++ PoryectoDiplomatura/src/main/java/com/ivanmancilla/PoryectoDiplomatura/PoryectoDiplomaturaApplication.java
@@ -0,0 +1,13 @@
+package com.ivanmancilla.PoryectoDiplomatura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoryectoDiplomaturaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoryectoDiplomaturaApplication.class, args);
+	}
+
+}
